a4f79e4af39fc6b589292868193283bb7cea1fa4
Dane Grant <danecando@gmail.com>
2015-06-15 11:23:35 -0400

    try again

--- deploy.php
+++ deploy.php
@@ -5,13 +5,12 @@ echo $test;
 $date = date_create();
 $payload = date_format($date, 'Y-m-d H:i:s') . "\n" .
            "=========== POST DATA ===========\n\n" .
-           print_r(json_decode($_POST)) .
+           print_r($_POST) .
            "\n\n=========== END POST DATA ===========\n";
 
 file_put_contents("./logs/deploy.log", $payload, FILE_APPEND);
 
-$pull_output = shell_exec("git pull origin production");
+$pull_output = shell_exec("git pull origin master");
 file_put_contents("./logs/deploy.log", $pull_output, FILE_APPEND);
 
 echo $pull_output;
-echo "\n updated again";
