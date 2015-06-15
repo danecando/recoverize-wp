f72cda636dec4af84db179d14dc130a39b0cb63e
Dane Grant <danecando@gmail.com>
2015-06-15 18:22:44 -0400

    updated deploy

--- deploy.php
+++ deploy.php
@@ -1,18 +1,15 @@
 <?php
-$test = file_put_contents("./logs/test.log", "testing");
-echo $test;
+//$date = date_create();
+//$payload = date_format($date, 'Y-m-d H:i:s') . "\n" .
+//           "=========== POST DATA ===========\n\n" .
+//           print_r($_POST) .
+//           "\n\n=========== END POST DATA ===========\n";
 
-$date = date_create();
-$payload = date_format($date, 'Y-m-d H:i:s') . "\n" .
-           "=========== POST DATA ===========\n\n" .
-           print_r($_POST) .
-           "\n\n=========== END POST DATA ===========\n";
-
-file_put_contents("./logs/deploy.log", $payload, FILE_APPEND);
+//file_put_contents("./logs/deploy.log", $payload, FILE_APPEND);
 
 $pull_output = shell_exec("git pull origin master");
 file_put_contents("./logs/deploy.log", $pull_output, FILE_APPEND);
 
 echo $pull_output;
 
-echo 'hooked';
+echo 'update test';
