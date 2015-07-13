63638d5d9749b31a769159f8651a618f0a73ff51
Dane Grant <danecando@gmail.com>
2015-07-13 10:20:41 -0400

    test

--- htdocs/wp-content/themes/listify/index.php
+++ htdocs/wp-content/themes/listify/index.php
@@ -10,7 +10,7 @@
  *
  * @package Listify
  */
-
+//
 get_header(); ?>
 
 	<?php if ( is_home() ) : ?>
--- permissions.sh
+++ permissions.sh
@@ -11,30 +11,34 @@ WP_ROOT=${WEB_ROOT}/htdocs # &lt;-- wordpress root directory
 WS_GROUP=www-data # &lt;-- webserver group
 
 # reset to safe defaults
-find ${WP_ROOT} -exec chown ${WP_OWNER}:${WP_GROUP} {} \;
-find ${WP_ROOT} -type d -exec chmod 755 {} \;
-find ${WP_ROOT} -type f -exec chmod 664 {} \;
+#find ${WP_ROOT} -exec chown ${WP_OWNER}:${WP_GROUP} {} \;
+#find ${WP_ROOT} -type d -exec chmod 755 {} \;
+#find ${WP_ROOT} -type f -exec chmod 664 {} \;
 
 # make sure repo is writeable
-find ${WEB_ROOT}/.git -type d -exec chmod 775 {} \;
-find ${WEB_ROOT}/.git -type f -exec chmod 664 {} \;
+#find ${WEB_ROOT}/.git -type d -exec chmod 775 {} \;
+#find ${WEB_ROOT}/.git -type f -exec chmod 664 {} \;
 
 # allow wordpress to manage wp-config.php (but prevent world access)
+chown -R rec:rec ${WEB_ROOT}
+find ${WEB_ROOT} -type d -exec chmod 775 {} \;
+find ${WEB_ROOT} -type f -exec chmod 664 {} \;
+
 chgrp ${WS_GROUP} ${WEB_ROOT}/wp-config.php
 chmod 660 ${WEB_ROOT}/wp-config.php
 
 # allow wordpress to manage .htaccess
-touch ${WP_ROOT}/.htaccess
-chgrp ${WS_GROUP} ${WP_ROOT}/.htaccess
-chmod 664 ${WP_ROOT}/.htaccess
+#touch ${WP_ROOT}/.htaccess
+#chgrp ${WS_GROUP} ${WP_ROOT}/.htaccess
+#chmod 664 ${WP_ROOT}/.htaccess
 
 # allow wordpress to manage wp-content
-find ${WP_ROOT}/wp-content -exec chgrp ${WS_GROUP} {} \;
-find ${WP_ROOT}/wp-content -type d -exec chmod 775 {} \;
-find ${WP_ROOT}/wp-content -type f -exec chmod 664 {} \;
+#find ${WP_ROOT}/wp-content -exec chgrp ${WS_GROUP} {} \;
+#find ${WP_ROOT}/wp-content -type d -exec chmod 775 {} \;
+#find ${WP_ROOT}/wp-content -type f -exec chmod 664 {} \;
 
 # allow write to logs directory
-chmod 775 ${WEB_ROOT}/logs
+#chmod 775 ${WEB_ROOT}/logs
 
 # allow write to cache
-chmod 777 ${WP_ROOT}/wp-content/cache
+#chmod 777 ${WP_ROOT}/wp-content/cache
