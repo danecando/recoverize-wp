fc625eb8e73003d4065875ac5dbc906b4505c205
Dane Grant <danecando@gmail.com>
2015-07-13 13:38:40 -0400

    permissions and ignore updates

--- permissions.sh
+++ permissions.sh
@@ -1,44 +1,26 @@
 #!/bin/bash
 #
-# This script configures WordPress file permissions based on recommendations
-# from http://codex.wordpress.org/Hardening_WordPress#File_permissions
+# This script configures WordPress file permissions
+#
+# www-data is in rec group & rec is in www-data group
+# umask 002 - permissions default to 775 & 664 (so group has write permissions)
+# www-data has home directory and login for ssh keys (git etc..)
 #
 
+# config
 WEB_ROOT=/home/rec/recoverize.com
-WP_OWNER=rec # &lt;-- wordpress owner
-WP_GROUP=rec # &lt;-- wordpress group
-WP_ROOT=${WEB_ROOT}/htdocs # &lt;-- wordpress root directory
-WS_GROUP=www-data # &lt;-- webserver group
+WP_OWNER=rec
+WP_GROUP=rec
+WP_ROOT=${WEB_ROOT}/htdocs
+WS_GROUP=www-data
 
-# reset to safe defaults
-#find ${WP_ROOT} -exec chown ${WP_OWNER}:${WP_GROUP} {} \;
-#find ${WP_ROOT} -type d -exec chmod 755 {} \;
-#find ${WP_ROOT} -type f -exec chmod 664 {} \;
-
-# make sure repo is writeable
-#find ${WEB_ROOT}/.git -type d -exec chmod 775 {} \;
-#find ${WEB_ROOT}/.git -type f -exec chmod 664 {} \;
-
-# allow wordpress to manage wp-config.php (but prevent world access)
+# make sure user owns website root and all contents
 chown -R rec:rec ${WEB_ROOT}
+
+# www-data in owner group has r+w permissions
 find ${WEB_ROOT} -type d -exec chmod 775 {} \;
 find ${WEB_ROOT} -type f -exec chmod 664 {} \;
 
+# sticter permissions on config file
 chgrp ${WS_GROUP} ${WEB_ROOT}/wp-config.php
 chmod 660 ${WEB_ROOT}/wp-config.php
-
-# allow wordpress to manage .htaccess
-#touch ${WP_ROOT}/.htaccess
-#chgrp ${WS_GROUP} ${WP_ROOT}/.htaccess
-#chmod 664 ${WP_ROOT}/.htaccess
-
-# allow wordpress to manage wp-content
-#find ${WP_ROOT}/wp-content -exec chgrp ${WS_GROUP} {} \;
-#find ${WP_ROOT}/wp-content -type d -exec chmod 775 {} \;
-#find ${WP_ROOT}/wp-content -type f -exec chmod 664 {} \;
-
-# allow write to logs directory
-#chmod 775 ${WEB_ROOT}/logs
-
-# allow write to cache
-#chmod 777 ${WP_ROOT}/wp-content/cache
